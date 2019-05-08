1a1ec610b88a57a1917d236ceb0976cf35acc441
Leo-Lijh <49966855+Leo-Lijh@users.noreply.github.com>
2019-05-08 08:24:16 +0800

    Update operDao.py

--- projectTest/MiddleProject/controller/operDao.py
+++ projectTest/MiddleProject/controller/operDao.py
@@ -3,7 +3,7 @@
     @version: python 3.7
     @time: 2019/4/30 22:01
     @author: YangZhen
-    @editor：LiJiaHe/GaoJiaYang
+    @editor：LiJiaHe-LeoLi/GaoJiaYang
     @title: 处理用户移动操作
     @file: gameController.py
 """
@@ -26,7 +26,28 @@ class UserDao:
         """
         self.__remove_row()
         self.__remove_col()
+        # 首先实现相邻横向相同元素合并
+    def merge(list_target):
+        for i in range(len(list_target)-1):
+            # 如果遇到左边或者右边有相同元素，则将元素变成0
+            # 【未完成】问题，如果i+2+3,或者i-2-3的数字都相等，怎么办？
+            if list_target[i] == list_target[i+1] or list_target[i] == list_target[i-1]:
+                list_target[i]
 
+        # 再将0元素统一放到最后，有几个零，依次从第一个15分，第二个加10分分别统计最后汇总，核算出本轮分数
+        # 【未完成】问题，核算出0的个数，如何进行分数累加汇总？如何将列表中0元素去除显示
+        zero_to_end(list_target)
+        # 核算出list_target中有多少个0元素，核算分数
+        num_zero == list_target.count(0)
+        # 再将零元素删除显示
+
+
+    def zero_to_end(list_target):
+        new_list = [item for item in list_target if item != 0]
+        new_list += [0]*list_target.count(0)
+        list_target[:] = new_list[:]
+        return list_target
+    
     def can_remove(self):
         """
         判断列表是否可以消除
